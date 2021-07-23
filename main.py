388b927d9b80e3f5fb07fa441b0480999cba5e1d
hhosseini1377 <hhosseini@ce.sharif.edu>
2021-07-23 19:17:29 +0430

    GUI window initialized

--- main.py
+++ main.py
@@ -5,4 +5,12 @@ expression = ""
 
 # Driver code
 if __name__ == "__main__":
-	pass
+
+	# set the background colour of GUI window
+    gui.configure(background="light green")
+ 
+    # set the title of GUI window
+    gui.title("Simple Calculator")
+ 
+    # set the configuration of GUI window
+    gui.geometry("270x150")
