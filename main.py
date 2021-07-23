e52a6a480497a4bf8c211bbf01e7a1054abe1a86
hhosseini1377 <hhosseini@ce.sharif.edu>
2021-07-23 19:21:35 +0430

    Expression fields added

--- main.py
+++ main.py
@@ -14,3 +14,11 @@ if __name__ == "__main__":
  
     # set the configuration of GUI window
     gui.geometry("270x150")
+
+    # StringVar() is the variable class
+    equation = StringVar()
+ 
+    # create the text entry box for showing the expression .
+    expression_field = Entry(gui, textvariable=equation)
+    expression_field.grid(columnspan=4, ipadx=70)
+ 
